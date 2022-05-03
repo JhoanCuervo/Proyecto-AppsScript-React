4fbe9d9716e65f51b0f84b90276fd1e7e6b6cbec
nikkocc <jhoancuervolozada@gmail.com>
2022-05-03 16:17:59 -0500

    Version 1

--- Código.js
+++
@@ -1,2 +0,0 @@
-const doGet = () => HtmlService.createTemplateFromFile('view/index').evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
-const include = (ruta) => HtmlService.createHtmlOutputFromFile(ruta).getContent();
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const doGet = () => 
+    HtmlService.createTemplateFromFile('frontend/index')
+        .evaluate()
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+        .addMetaTag(
+            "viewport",
+            'width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"'
+        )
+        .setTitle("Proyecto apps script dev")
+        .setFaviconUrl("https://cdn-icons-png.flaticon.com/512/6792/6792355.png");
+const include = (ruta) => HtmlService.createHtmlOutputFromFile(ruta).getContent();
